173bf0ae52f7ea587dfef5e5c20302f38e191b2c
Kevin Tou <toukevin@gmail.com>
2019-08-20 12:06:56 -0700

    Set up basic redux store

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+  friends: [],
+};
+
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
